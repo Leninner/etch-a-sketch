ca6b73f748146d9124bc480f1b8ce8408d379e9f
Leninner <Mazabandalenin180@gmail.com>
2021-04-25 22:29:50 -0500

    Añadimos la manera de hacer los cuadros que deseamos con operaciones en la función 'escucha'

--- juice.js
+++ juice.js
@@ -1,14 +1,27 @@
 const contenedor = document.querySelector('#contenedor');
 let div = document.createElement('div');
+const btn = document.getElementById('btn');
+btn.style.backgroundColor  = "black";
+btn.style.width = "150px";
+btn.style.marginLeft = "600px";
+btn.style.marginTop = "50px";
 
-    crearCajas();
+btn.addEventListener('click', function (e){
+    console.log(e.target);
+});
+
+crearCajas();
 
 function crearCajas(){
-    for(let i=0; i<256; i++){
+    //Creamos la manera de hacer los cuadros que queramos
+    //Basta con ajustar el tamaño del contenedor general y 
+    //hacer operaciones simples para encontrar el nuevo
+    //tamaño de los child divs.
+    for(let i=0; i<(16*16); i++){
     div = document.createElement('div');
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.backgroundColor = "Red";
+    div.style.width = ((400/16)-2)+"px";
+    div.style.height = ((400/16)-2)+"px";
+    div.style.backgroundColor = "white";
     div.style.float = "left";
     div.style.margin = "0px";
     div.style.border = "0.5px solid rgba(0, 0, 0, 0.8)";
@@ -17,8 +30,9 @@ function crearCajas(){
 }
 
 contenedor.addEventListener('mouseover', function (e){
-    e.target.style.backgroundColor = "GREEN";
+    e.target.style.backgroundColor = "Yellow";
     // console.log(div);
     // console.log(e.target);
 });
+
 
